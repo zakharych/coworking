1e5e300573219004c2d882081aa9f7a62c3dfeaa
alexzlk <alexzlk@yandex.ru>
2022-03-06 16:17:01 +0300

    Add header menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+import { Layout, Menu, Breadcrumb, Button } from "antd";
+
+import { ReactComponent as ReactLogo } from "./components/logo.svg";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section-header">
+        <div className="section_wrapper">
+          <header className="header">
+            <ReactLogo className="logo1" />
+            <ul className="header__menu">
+              <li className="header__item">Акции</li>
+              <li className="header__item">Что включено</li>
+              <li className="header__item">Цены</li>
+              <li className="header__item">Как пользоваться?</li>
+              <li className="header__item">Контакты</li>
+            </ul>
+            <a href="tel:+78452496987" className="telephone">
+              +7 (8452) 49 69 87
+            </a>
+            <Button>Вход</Button>
+          </header>
+        </div>
+      </div>
     </div>
   );
 }
