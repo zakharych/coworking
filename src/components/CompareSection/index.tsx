82d51c80f11d0a73c4abc8a5e8a603f1e2215096
alexzlk <alexzlk@yandex.ru>
2022-03-13 13:59:18 +0300

    Update Compare Section

--- src/components/CompareSection/index.tsx
+++ src/components/CompareSection/index.tsx
@@ -3,6 +3,7 @@ import classNames from "classnames";
 import Section from "../Section";
 import s from "./CompareSection.module.scss";
 import Diagram from "./images/diagram.png";
+import FirstPromo from "./images/first_promo.jpeg";
 import Cafe from "./images/cafe.png";
 import { ReactComponent as NegationIcon } from "./images/negationIcon.svg";
 
@@ -11,7 +12,9 @@ console.log(s);
 export default function CompareSection() {
   return (
     <Section className={s["compare-section"]}>
-      <div className="section_wrapper">
+      <div
+        className={classNames("section_wrapper", s["compare-section-wrapper"])}
+      >
         <Row gutter={[30, 0]} className={s["diagram-compare"]}>
           <Col span={12} className={s["compare-diagram"]}>
             <div className={classNames(s["compare-diagram-title"])}>
@@ -80,70 +83,134 @@ export default function CompareSection() {
             </div>
           </Col>
         </Row>
-        <div className={classNames("section-title", s["compare-cards-title"])}>
-          Не ходите в коворкинг -
-          <span className="text-accent">не платите!</span>
+        <div className={s["compare-cards-wrapper"]}>
+          <div
+            className={classNames("section-title", s["compare-cards-title"])}
+          >
+            Не ходите в коворкинг -
+            <span className="text-accent">не платите!</span>
+          </div>
+          <Row gutter={[30, 0]} className={s["compare-cards"]}>
+            {new Array(2).fill(null).map((_, index) => {
+              return (
+                <Col span={12}>
+                  <div className={s["compare-card"]}>
+                    <div className={s["compare-card__header-title"]}>
+                      <span className="text-accent">Думаете</span>, работать из
+                      кафе удобнее?
+                    </div>
+                    <div className={s["compare-card-pic"]}>
+                      <img
+                        src={Cafe}
+                        alt=""
+                        className={s["compare-card-img"]}
+                      />
+                    </div>
+                    <div className={s["compare-card__title"]}>
+                      Если вы думаете, что можете бесплатно просидеть в кафе
+                      каждый день и не оставить там ни копейки - вы ошибаетесь.{" "}
+                      <span className="text-accent-bold">
+                        Обычно, фрилансер оставляет там от 600 рублей в день.
+                      </span>
+                    </div>
+                    <div className={s["compare-card__subtitle"]}>
+                      Также в кафе вы столкнетесь с такими проблемами как:
+                    </div>
+                    <ul className={s["negation-list"]}>
+                      <li className={s["negation-item"]}>
+                        <NegationIcon className={s["negation-icon"]} />
+                        <div className={s["negation-item__title"]}>
+                          Отсутствие качественного интернета
+                        </div>
+                      </li>
+                      <li className={s["negation-item"]}>
+                        <NegationIcon className={s["negation-icon"]} />
+                        <div className={s["negation-item__title"]}>
+                          Не везде есть розетки
+                        </div>
+                      </li>
+                      <li className={s["negation-item"]}>
+                        <NegationIcon className={s["negation-icon"]} />
+                        <div className={s["negation-item__title"]}>
+                          Музыка и разговоры на фоне
+                        </div>
+                      </li>
+                      <li className={s["negation-item"]}>
+                        <NegationIcon className={s["negation-icon"]} />
+                        <div className={s["negation-item__title"]}>
+                          Косые взгляды официантов, так как им не выгодно, чтобы
+                          вы занимали столик весь день, заказав только один кофе
+                        </div>
+                      </li>
+                      <li className={s["negation-item"]}>
+                        <NegationIcon className={s["negation-icon"]} />
+                        <div className={s["negation-item__title"]}>
+                          Отсутствие рабочей атмосферы и как результат, падает
+                          ваша концентрация
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+          <button className="Button">Попробовать коворкинг</button>
         </div>
-        <Row gutter={[30, 0]} className={s["compare-cards"]}>
-          {new Array(2).fill(null).map((_, index) => {
-            return (
-              <Col span={12}>
-                <div className={s["compare-card"]}>
-                  <div className={s["compare-card__header-title"]}>
-                    <span className="text-accent">Думаете</span>, работать из
-                    кафе удобнее?
-                  </div>
-                  <div className={s["compare-card-pic"]}>
-                    <img src={Cafe} alt="" className={s["compare-card-img"]} />
-                  </div>
-                  <div className={s["compare-card__title"]}>
-                    Если вы думаете, что можете бесплатно просидеть в кафе
-                    каждый день и не оставить там ни копейки - вы ошибаетесь.{" "}
-                    <span className="text-accent-bold">
-                      Обычно, фрилансер оставляет там от 600 рублей в день.
-                    </span>
-                  </div>
-                  <div className={s["compare-card__subtitle"]}>
-                    Также в кафе вы столкнетесь с такими проблемами как:
-                  </div>
-                  <ul className={s["negation-list"]}>
-                    <li className={s["negation-item"]}>
-                      <NegationIcon className={s["negation-icon"]} />
-                      <div className={s["negation-item__title"]}>
-                        Отсутствие качественного интернета
-                      </div>
-                    </li>
-                    <li className={s["negation-item"]}>
-                      <NegationIcon className={s["negation-icon"]} />
-                      <div className={s["negation-item__title"]}>
-                        Не везде есть розетки
-                      </div>
-                    </li>
-                    <li className={s["negation-item"]}>
-                      <NegationIcon className={s["negation-icon"]} />
-                      <div className={s["negation-item__title"]}>
-                        Музыка и разговоры на фоне
-                      </div>
-                    </li>
-                    <li className={s["negation-item"]}>
-                      <NegationIcon className={s["negation-icon"]} />
-                      <div className={s["negation-item__title"]}>
-                        Косые взгляды официантов, так как им не выгодно, чтобы
-                        вы занимали столик весь день, заказав только один кофе
-                      </div>
-                    </li>
-                    <li className={s["negation-item"]}>
-                      <NegationIcon className={s["negation-icon"]} />
-                      <div className={s["negation-item__title"]}>
-                        Отсутствие рабочей атмосферы и как результат, падает
-                        ваша концентрация
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </Col>
-            );
-          })}
+
+        <div className={s["first-promo-title-wrapper"]}>
+          <div className={classNames("section-title", s["first-promo-title"])}>
+            НЕ УВЕРЕНЫ,
+            <span className="text-accent"> ЧТО ВАМ ПОДОЙДЕТ КОВОРКИНГ?</span>
+          </div>
+          <div
+            className={classNames(
+              s["first-promo-subtitle"],
+              "text-accent-bold"
+            )}
+          >
+            лучше один раз увидеть, чем 100 раз услышать!
+          </div>
+        </div>
+        <div className={s["first-promo"]}></div>
+        <Row className={s["first-promo"]}>
+          <Col span={12}>
+            <div className={s["first-promo__pic"]}>
+              <img
+                src={FirstPromo}
+                alt=""
+                className={s["first-promo__img"]}
+              ></img>
+            </div>
+          </Col>
+          <Col span={12}>
+            <div className={s["first-promo__text-content"]}>
+              <div
+                className={classNames(
+                  s["first-promo__text-content-title"],
+                  "section-title"
+                )}
+              >
+                Первое полноценное посещение коворкинга АБСОЛЮТНО{" "}
+                <span className="text-accent">бесплатно!</span>
+              </div>
+              <div className={classNames(s["first-promo-subtitle"])}>
+                Просто приходите в любой удобный вам день, и администратор сразу
+                выдаст вам промокод на <span className="text-accent">*</span>
+                бесплатное посещение!
+              </div>
+              <div
+                className={classNames(
+                  s["first-promo-subtitle"],
+                  s["first-promo-subtitle--addition"]
+                )}
+              >
+                <span className="text-accent">* </span>акция действует в обоих
+                коворкингах, вы можете получить по одному дню в альфа и бета
+                коворкингах
+              </div>
+            </div>
+          </Col>
         </Row>
       </div>
     </Section>
